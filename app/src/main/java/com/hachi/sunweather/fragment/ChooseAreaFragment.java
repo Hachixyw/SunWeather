9ea2c7cc4e9ab7a753691e13a521ff3b09d8c3a4
Hachi <1298303257@qq.com>
2019-06-12 18:57:03 +0800

    增加切换城市和更新天气

--- app/src/main/java/com/hachi/sunweather/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/hachi/sunweather/fragment/ChooseAreaFragment.java
@@ -17,6 +17,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.hachi.sunweather.MainActivity;
 import com.hachi.sunweather.R;
 import com.hachi.sunweather.WeatherActivity;
 import com.hachi.sunweather.db.City;
@@ -83,10 +84,18 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if (currentLevel==LEVEL_COUNTY){
                     String weatherId=countyList.get(position).getWeatherId();
-                    Intent intent=new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity){
+                        Intent intent=new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity=(WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefreshLayout.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
